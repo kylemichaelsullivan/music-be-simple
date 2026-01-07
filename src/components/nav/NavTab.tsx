0de13a2d5a2faa6b83deb9e8901da7fdb9a0a9fb
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2026-01-06 20:58:07 -0500

    FIX: <NavTab /> Responsive Design

--- src/components/nav/NavTab.tsx
+++ src/components/nav/NavTab.tsx
@@ -11,12 +11,12 @@ export default function NavTab({ title, to }: NavTabProps) {
 	return (
 		<Link
 			to={to}
-			className='flex flex-1 gap-2 border border-gray-500 rounded-lg transition-colors px-4 py-2 hover:bg-gray-100 sm:border-b-0 sm:rounded-b-none'
+			className='NavTab flex flex-1 justify-center gap-2 items-center border border-gray-500 rounded-lg transition-colors px-4 py-2 hover:bg-gray-100 sm:border-b-0 sm:rounded-b-none'
 			activeProps={{ className: 'bg-gray-300 font-bold' }}
 			title={title}
 		>
 			<NavIcon name={title} />
-			<span className='hidden sm:block'>{title}</span>
+			<span>{title}</span>
 		</Link>
 	);
 }
