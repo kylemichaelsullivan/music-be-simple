737a5ee42fdfbd3f075b1a574b2f67131c764c57
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2026-01-11 02:09:18 -0500

    ADD: <EditButton />

---
+++ src/components/buttons/EditButton.tsx
@@ -0,0 +1,15 @@
+import { ActionIcon } from '@/components/icons';
+import { IconButton } from './IconButton';
+
+type EditButtonProps = {
+	onFxn: () => void;
+	title?: string;
+};
+
+export function EditButton({ title = 'Edit', onFxn }: EditButtonProps) {
+	return (
+		<IconButton className='EditButton absolute top-1 left-1 p-2' title={title} onFxn={onFxn}>
+			<ActionIcon name='pen' size='xs' />
+		</IconButton>
+	);
+}
