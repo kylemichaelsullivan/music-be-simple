05af9cac70a4e18e5ddbf00270c07ad0db622f8f
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2026-01-15 00:11:09 -0500

    UPDATE: <ChordBinItems /> Empty Fallback

--- src/pages/Play/ChordBin/ChordBinItems.tsx
+++ src/pages/Play/ChordBin/ChordBinItems.tsx
@@ -6,9 +6,13 @@ export function ChordBinItems() {
 
 	return (
 		<div className='ChordBinItems grid gap-4'>
-			{chordBinItems.map((item) => (
-				<ChordBinItem item={item} onRemove={() => removeChordBinItem(item.id)} key={item.id} />
-			))}
+			{chordBinItems.length > 0 ? (
+				chordBinItems.map((item) => (
+					<ChordBinItem item={item} onRemove={() => removeChordBinItem(item.id)} key={item.id} />
+				))
+			) : (
+				<span className='text-sm italic text-center'>add a chord</span>
+			)}
 		</div>
 	);
 }
